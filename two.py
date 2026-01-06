2dab9094ff98866a6017a40670ec4d7842099a5e
manvar kangna <manvarkangna@gmail.com>
2026-01-05 23:25:03 -0800

    Add files via upload

---
+++ two.py
@@ -0,0 +1,16 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> str("hello world")
+'hello world'
+>>> int("20")
+20
+>>> float("20.5")
+20.5
+>>> complex("1j")
+1j
+>>> list["apple","banana","cherry"]
+list['apple', 'banana', 'cherry']
+>>> frozenset({"apple","banana","cherry"})
+frozenset({'banana', 'apple', 'cherry'})
+>>> bool("true")
+True
