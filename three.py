9914a6829c6570dc8657c9581b1c77e4f72d999b
manvar kangna <manvarkangna@gmail.com>
2026-01-07 07:02:37 -0800

    Add files via upload

---
+++ three.py
@@ -0,0 +1,12 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print("hello")
+hello
+>>> print(" i am kangna")
+ i am kangna
+>>> print("i am from gondal")
+i am from gondal
+>>> print("i am studying in bsc it")
+i am studying in bsc it
+>>> print("i am study at atmiya university")
+i am study at atmiya university
