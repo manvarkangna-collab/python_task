114a9db3b0a606162929c6c7cbc03b7e85ee7992
manvar kangna <manvarkangna@gmail.com>
2026-01-26 22:43:55 -0500

    Add files via upload

---
+++ task.2.py.py
@@ -0,0 +1,73 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+a=5
+b=10
+print(a+b)
+15
+print(a-b)
+-5
+print(a/b)
+0.5
+x=10
+print(x)
+10
+a=5
+print(-a)
+-5
+p=10
+q=5
+print(p>q)
+True
+print(p==q)
+False
+x=true
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    x=true
+NameError: name 'true' is not defined. Did you mean: 'True'?
+y=false
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    y=false
+NameError: name 'false' is not defined. Did you mean: 'False'?
+>>> print(x and y)
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    print(x and y)
+NameError: name 'y' is not defined
+>>> print(xor y)
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+>>> print(not x)
+False
+>>> is_pass
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    is_pass
+NameError: name 'is_pass' is not defined
+>>> print(is_pass)
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    print(is_pass)
+NameError: name 'is_pass' is not defined
+>>> a=5
+>>> b=3
+>>> print(a and b)
+3
+>>> print(a|b)
+7
+>>> numbers=[1,2,3,4]
+>>> print(2 is number)
+Traceback (most recent call last):
+  File "<pyshell#25>", line 1, in <module>
+    print(2 is number)
+NameError: name 'number' is not defined. Did you mean: 'numbers'?
+>>> print(5 not in number)
+Traceback (most recent call last):
+  File "<pyshell#26>", line 1, in <module>
+    print(5 not in number)
+NameError: name 'number' is not defined. Did you mean: 'numbers'?
+>>> x=10
+>>> y=10
+>>> print(xis y0
+...       
+SyntaxError: '(' was never closed
