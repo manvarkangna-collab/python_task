7c5b3e894f7abf48493f72fe47f83e4018dd54f4
manvar kangna <manvarkangna@gmail.com>
2026-01-30 21:42:47 -0500

    Add files via upload

---
+++ io.py
@@ -0,0 +1,10 @@
+"""name=input("enter your name:")
+print("hello world")
+num=int(input("enter an integer:"))
+print("number=",num)"""
+#output command
+print("hello,world!")
+name="ABC"
+age=15
+print("name:",name)
+print("age:",age)
