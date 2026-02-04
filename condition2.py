2432ce598da880eb9b6c734125efd81cfc00540c
manvar kangna <manvarkangna@gmail.com>
2026-02-04 05:33:53 -0500

    Add files via upload

---
+++ condition2.py
@@ -0,0 +1,8 @@
+marks=int(input( "enter your marks:"))
+if marks>=90:
+    print("you will get a grade:")
+elif marks>=70:
+     print("you will get b grade:")
+else:
+     print("you will get c grade:")
+
