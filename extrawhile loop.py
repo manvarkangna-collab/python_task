bc4cc969ace333794c199eee6b3214dfc0affecd
manvar kangna <manvarkangna@gmail.com>
2026-02-24 08:04:52 +0530

    Add files via upload

---
+++ extrawhile loop.py
@@ -0,0 +1,24 @@
+'''for i in range(1, 6):
+    print(i)'''
+'''user_input = input("Enter numbers separated by spaces: ")
+numbers = [float(x) for x in user_input.split()]
+total_sum = sum(numbers)
+print(f"The total sum is: {total_sum}")'''
+'''print("Odd numbers:")
+for i in range(1, 21):
+    if i % 2 != 0:
+        print(i, end=" ")
+print() # New line'''
+'''print("Table of 4:")
+for i in range(1, 11):
+    print(f"4 x {i} = {4 * i}")'''
+'''for i in range(5, 0, -1):
+    print(i, end="")
+print() # New line'''
+'''my_list = [23, 89, 12, 56, 7, 99, 45]
+largest = max(my_list)
+print(f"The largest number in {my_list} is: {largest}")'''
+'''print("Even numbers:")
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i, end=" ")'''
