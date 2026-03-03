651bc790fd125bc13a89a0e1c6411f41ce08f9ff
manvar kangna <manvarkangna@gmail.com>
2026-03-03 08:23:07 +0530

    Add files via upload

---
+++ create impoort.py
@@ -0,0 +1,13 @@
+# integer array
+'''from array import array
+ arr=array('i',[10,20,30,40])
+   print(arr)
+   print(type(arr))'''
+ # float array
+ '''from array import array
+ arr=array('f',[1.5,2.5,3.5,4.5])
+   print(arr)'''
+ # empty array
+'''from array import array
+ arr=array('i',[])
+   print(len(arr))'''
