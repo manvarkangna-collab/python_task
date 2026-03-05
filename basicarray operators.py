7124e45de95dd6fb9a9bcae42776de26de7441f4
manvar kangna <manvarkangna@gmail.com>
2026-03-05 10:40:57 +0530

    Add files via upload

---
+++ basicarray operators.py
@@ -0,0 +1,31 @@
+#len()-number of elements
+'''import array
+arr=array('i'[10,20,30,40,50])
+print(len(arr))'''
+#append(x)-add element at end
+'''arr=array('i'[10,20,30])
+arr.append(40)
+print(arr)'''
+#insert(pop,x)-insert at position
+'''arr=array('i'[10,20,40])
+arr.insert(2,30)
+print(arr)'''
+#remove(x)-remove first occurance
+'''arr=array('i'[10,20,30,20,40])
+arr.remove(20)
+print(arr)'''
+#pop()-remove and return last element
+'''arr=array('i',[10,20,30,40,])
+x= arr.pop()
+print("removed:",x)
+print(arr)'''
+#index(x)-find index of elements
+'''arr=array('i',[10,20,30,40,])
+print(arr.index(30))'''
+#count(x)-copunt occurances
+'''arr=array('i',[10,20,30,20,40,])
+print(arr.count(20))'''
+#reverse()-reverse array
+'''arr=array('i'[10,20,30,40])
+arr.reverse()
+print(arr)'''
