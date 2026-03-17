e22a5525714f9d08f22dfb2d798141f89f23f177
manvar kangna <manvarkangna@gmail.com>
2026-03-17 09:08:33 +0530

    Add files via upload

---
+++ default.py
@@ -0,0 +1,23 @@
+#basic default
+'''def greet(name,msg="good morning");
+greet("isha")
+greet("riya","good evening")'''
+
+
+#power function
+'''def power(num exp=2);
+return num**except
+print(power(3))
+print(power(3,3))
+print(power(2,4))'''
+
+
+#multiple defaault
+'''def student_info(name,age=18,course="bsc it");
+print("name",name)
+print("age",age)
+print("course",course)
+student_info("ravi")
+student_info("seema"20)
+student_info("amit",19,"bscit")'''
+
