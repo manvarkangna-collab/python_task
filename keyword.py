e4b0454920ef2b731d20f0cbce424b87e477b0a0
manvar kangna <manvarkangna@gmail.com>
2026-03-17 08:56:39 +0530

    Add files via upload

---
+++ keyword.py
@@ -0,0 +1,24 @@
+#basic keyword 
+'''def student_info(name,age,city);
+print("name",name)
+print("age",age)
+print("city",city)
+student_info (age=18,cituy="rajkot" ,name="ravi")'''
+
+
+#mixing positional
+'''def displau(a,b,c);
+print("a=",a)
+print("b=",b)
+print("c=",c)
+display(1,c=3,b=2)'''
+ 
+
+# intrest
+'''def  simple intrest 
+(p:float,r:int,t:float)
+si=(p*r*t)/100
+print("simple intrest5:",si)
+simple_intrest(p=10000,t=2,r=1.5)
+simple_intrest(t=1.5,p=15000,r=2)'''
+
