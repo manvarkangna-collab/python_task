067e437f6535da711f22aa807e2f181060202257
manvar kangna <manvarkangna@gmail.com>
2026-03-28 20:50:20 +0530

    Add files via upload

---
+++ copy and merge.py
@@ -0,0 +1,20 @@
+# copy content from one file to anothere
+'''src=open("d1.txt","r")
+data=src.read()
+src close()
+
+dst=open("bit.txt","w")
+dst.write(data)
+dst.close()
+print("file copied succesfully")'''
+
+
+
+#merge two files into a third file
+'''with open("d1.txt","r") as f1.open("data.txt","r") as f2 open("create.txt","w") as f3:
+f3.write(f1.read())
+f3.write("\n")
+f3.write(f2.read())
+print("file copied succesfully")'''
+
+
