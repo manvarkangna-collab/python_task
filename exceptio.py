d7d259df172890e9c5716d5cec1da94054d62b79
manvar kangna <manvarkangna@gmail.com>
2026-04-04 08:54:55 +0530

    Add files via upload

---
+++ exceptio.py
@@ -0,0 +1,16 @@
+try :
+number1=int(input("enter number:"))
+number2=int(input("enter anothere number:"))
+result=number1/number2
+
+except:zero division error:
+print("you cannot divide by zero!")
+
+ except: value error: 
+ print("please enter a valid number")
+
+ else:
+print("division succefully result is:",result) 
+
+finally:
+ print("this block always run")
